5b46e4dbe8a86a14bc63e99dc73317b16794e973
Jetrca92 <blaz.guzej@gmail.com>
2023-02-18 11:29:44 +0100

    Add functionality to buttons

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+// Functions for basic math operators
 function add(x, y) {
     return x + y;
 }
@@ -14,33 +15,83 @@ function divide(x, y) {
     return x / y;
 }
 
+// Calls functions for different operators
 function operate(operator, x, y) {
     if (operator === "+") {
-        add(x, y);
+        return parseFloat((add(x, y)).toFixed(5));
     }
     else if (operator === "-") {
-        substract(x, y);
+        return parseFloat((substract(x, y)).toFixed(5));
     }
-    else if (operator === "*") {
-        multiply(x, y);
+    else if (operator === "x") {
+        return parseFloat((multiply(x, y)).toFixed(5));
     }
     else if (operator === "÷") {
-        divide(x, y);
+        return parseFloat((divide(x, y)).toFixed(5));
     } 
 }
-let displayValue = "";
+
+// Add event listeners to buttons, store data in variables and display on screen
 const buttons = document.querySelectorAll('.btn');
+const screenLarge = document.querySelector('.screen-large');
+const screenSmall = document.querySelector('.screen-small');
+let displayValue = "";
+let x;
+let y;
+let op;
 buttons.forEach(button => button.addEventListener('click', function () {
     const data = button.dataset.number;
-    if (data) {
-        const screenLarge = document.querySelector('.screen-large');
-        screenLarge.innerHTML += data;
-        displayValue += data;
-    }
     const operator = button.dataset.operator;
+    if (data) {
+        screenLarge.innerHTML = displayValue;
+        screenLarge.innerHTML += data;
+        displayValue += data
+        
+    }
     if (operator) {
-        const screenSmall = document.querySelector('.screen-small');
         screenSmall.innerHTML = displayValue + " " + operator;
+        screenLarge.innerHTML = displayValue;
+        op = operator;
+        
+        // Store numbers before the operator as x, clear displayValue
+        x = parseFloat(displayValue);
+        displayValue = "";
+    }
+    if (button.id === "equalsBtn") {
+        // Prevents error from presing equalsBtn many times
+        if (!op) {
+            return;
+        }
+        // Store y
+        y = parseFloat(displayValue);
+        displayValue = operate(op, x, y);
+        screenLarge.innerHTML = displayValue;
+        screenSmall.innerHTML = x + " " + op + " " + y + " = " + displayValue; 
+        y = undefined;
+        op = undefined;
+    }
+
+    // Dot button
+    if (button.id === "dotBtn") {
+        displayValue += ".";
+        screenLarge.innerHTML += ".";
+    }
+
+    // Clear
+    if (button.id === "clearBtn") {
+        displayValue = "";
+        x = undefined;
+        y = undefined;
+        op = undefined;
+        screenLarge.innerHTML = "";
+        screenSmall.innerHTML = "";
     } 
+
+    // Delete 
+    if (button.id === "deleteBtn") {
+        screenLarge.innerHTML = "";
+        displayValue = "";
+        x = undefined;
+    }
     
 }));
